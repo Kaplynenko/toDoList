47134fa9f9da55c60198f823d9a93253fcc943dc
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-07-20 09:57:28 +0300

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import Container from "./Container";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+     <Container/>
     </div>
   );
 };
